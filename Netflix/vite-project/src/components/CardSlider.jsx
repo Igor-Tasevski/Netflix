cd119a83652a726d781f758a11636a09fb1ad2a0
Igor-Tasevski <igortasevski22@gmail.com>
2024-12-01 13:59:58 +0100

    Netflix-clone

---
+++ Netflix/vite-project/src/components/CardSlider.jsx
@@ -0,0 +1,44 @@
+import './CardSlider.css'
+import { BASE_URL } from '../App'
+import React from 'react'
+//import { UseHover } from '../CustomHook/UseHover'
+
+
+
+function CardSlider({ title, data }) {
+
+   {/* const [hoverRef, isHovered] = UseHover()  */}
+
+    return (
+        <div className='cardSlider--view'>
+            <h4>{title}</h4>
+
+            <div className='slider-car-show'>
+
+             {/* <div ref={hoverRef}> */ }
+                    {/* {isHovered ? "You did it!" : " false!"} */}
+                    {data?.map((movieList) => (
+                        <div key={movieList.id} className='slider'>
+                            <img src={BASE_URL + movieList.poster_path} />
+                        </div>
+                    ))}
+            {/*   </div>  */}
+
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default CardSlider
+
+
+
+
+
+
+
+
+
